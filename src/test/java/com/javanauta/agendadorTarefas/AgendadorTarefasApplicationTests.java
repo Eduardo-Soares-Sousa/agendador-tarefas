f6f80dde35c0da244a0ec68e7039bfc465ac40b5
Eduardo Soares <eduardo_soares001@outlook.com>
2026-02-08 15:32:48 -0300

    commit inicial

---
+++ src/test/java/com/javanauta/agendadorTarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendadorTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
