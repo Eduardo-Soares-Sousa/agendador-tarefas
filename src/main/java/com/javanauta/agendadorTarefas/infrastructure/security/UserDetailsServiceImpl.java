53c4d9ea08c843a27fc682da7c158111191ce1d9
Eduardo Soares <eduardo_soares001@outlook.com>
2026-02-08 15:59:07 -0300

    adicionando configurações de segurança

---
+++ src/main/java/com/javanauta/agendadorTarefas/infrastructure/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.javanauta.agendadorTarefas.infrastructure.security;
+
+import com.javanauta.agendadorTarefas.business.dto.UsuarioDto;
+import com.javanauta.agendadorTarefas.infrastructure.client.UsuarioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl {
+    @Autowired
+    private UsuarioClient usuarioClient;
+
+    public UserDetails carregaDadosUsuario(String email, String token) {
+        UsuarioDto usuarioDto = usuarioClient.buscaUsuarioPorEmail(email, token);
+
+        return User
+                .withUsername(usuarioDto.getEmail()) // Define o nome de usuário como o e-mail
+                .password(usuarioDto.getSenha()) // Define a senha do usuário
+                .build();
+    }
+}
