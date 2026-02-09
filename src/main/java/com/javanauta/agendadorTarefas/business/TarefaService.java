ae8018988b011cb8e9d793d3d7b8633ae4d54089
Eduardo Soares <eduardo_soares001@outlook.com>
2026-02-08 22:03:33 -0300

    adicionados metodos de delete, patch de status e put de tarefas

--- src/main/java/com/javanauta/agendadorTarefas/business/TarefaService.java
+++ src/main/java/com/javanauta/agendadorTarefas/business/TarefaService.java
@@ -2,7 +2,10 @@ package com.javanauta.agendadorTarefas.business;
 
 import com.javanauta.agendadorTarefas.business.dto.TarefaDto;
 import com.javanauta.agendadorTarefas.business.mapper.TarefaConverter;
+import com.javanauta.agendadorTarefas.business.mapper.TarefaUpdateConverter;
+import com.javanauta.agendadorTarefas.infrastructure.entity.Tarefa;
 import com.javanauta.agendadorTarefas.infrastructure.enums.Status;
+import com.javanauta.agendadorTarefas.infrastructure.exceptions.ResourceNotFoundException;
 import com.javanauta.agendadorTarefas.infrastructure.repository.TarefaRepository;
 import com.javanauta.agendadorTarefas.infrastructure.security.JwtUtil;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +19,7 @@ import java.util.List;
 public class TarefaService {
     private final TarefaRepository tarefaRepository;
     private final TarefaConverter tarefaConverter;
+    private final TarefaUpdateConverter tarefaUpdateConverter;
     private final JwtUtil jwtUtil;
 
     public TarefaDto salvarTarefa(String token, TarefaDto tarefaDto) {
@@ -44,5 +48,38 @@ public class TarefaService {
         return tarefaConverter.paraListaTarefaDto(
                 tarefaRepository.findByEmailUsuario(email)
         );
+    }
+
+    public void deletarTarefaPorId(String id) {
+        tarefaRepository.deleteById(id);
+    }
+
+    public TarefaDto atualizaStatus(Status status, String id) {
+        try {
+            Tarefa tarefa = tarefaRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException(
+                            "Tarefa não encontrada. O ID " + id + " não foi localizado"
+                    ));
+
+            tarefa.setStatus(status);
+
+            return tarefaConverter.paraTarefaDto(tarefaRepository.save(tarefa));
+        }catch(ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Erro ao alterar status da tarefa " + e.getCause());
+        }
+    }
+
+    public TarefaDto atualizarDadosTarefa(TarefaDto tarefaDto, String id) {
+        try {
+            Tarefa tarefa = tarefaRepository.findById(id)
+                    .orElseThrow(() -> new ResourceNotFoundException(
+                            "Tarefa não encontrada. O ID " + id + " não foi localizado"
+                    ));
+
+            tarefaUpdateConverter.updateTarefas(tarefaDto, tarefa);
+            return tarefaConverter.paraTarefaDto(tarefaRepository.save(tarefa));
+        }catch(ResourceNotFoundException e) {
+            throw new ResourceNotFoundException("Erro ao alterar status da tarefa " + e.getCause());
+        }
     }
 }
--- src/main/java/com/javanauta/agendadorTarefas/controller/TarefaController.java
+++ src/main/java/com/javanauta/agendadorTarefas/controller/TarefaController.java
@@ -2,11 +2,16 @@ package com.javanauta.agendadorTarefas.controller;
 
 import com.javanauta.agendadorTarefas.business.TarefaService;
 import com.javanauta.agendadorTarefas.business.dto.TarefaDto;
+import com.javanauta.agendadorTarefas.infrastructure.enums.Status;
+import com.javanauta.agendadorTarefas.infrastructure.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,5 +45,30 @@ public class TarefaController {
     public ResponseEntity<List<TarefaDto>> buscarTarefasPorEmailUsuario(
             @RequestHeader("Authorization") String token) {
         return ResponseEntity.ok(tarefaService.buscarTarefasPorEmailUsuario(token));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletarTarefaPorId(@RequestParam("id") String id) {
+        try {
+            tarefaService.deletarTarefaPorId(id);
+        }catch(ResourceNotFoundException e) {
+            throw new ResourceNotFoundException(
+                    "Erro ao deletar tarefa. O ID " + id + " não foi localizado", e.getCause()
+            );
+        }
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<TarefaDto> atualizaStatus(@RequestParam("status") Status status,
+                                                    @RequestParam("id") String id) {
+        return ResponseEntity.ok(tarefaService.atualizaStatus(status, id));
+    }
+
+    @PutMapping
+    public ResponseEntity<TarefaDto> atualizaDadosTarefa(@RequestBody TarefaDto tarefaDto,
+                                                         @RequestParam("id") String id) {
+        return ResponseEntity.ok(tarefaService.atualizarDadosTarefa(tarefaDto, id));
     }
 }
